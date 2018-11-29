ae86bed5f87b51313903ab97a4a2f156009fd7e1
Ryan Cheley <rcheley@gmail.com>
2018-11-28 19:09:16 -0800

    Added Bash version of Fizz Buzz

---
+++ fizz-buzz.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+echo "Enter a Number: " 
+
+read my_number
+
+re='^[+-]?[0-9]+$'
+if ! [[ $my_number =~ $re ]] ; then
+   echo "error: Not a number" >&2; exit 1
+fi
+
+if ! ((my_number % 3)) && ((my_number % 15)); then
+	echo "fizz"
+elif ! ((my_number % 5)) && ((my_number % 15)); then
+	echo "buzz"
+elif ! ((my_number % 15)) ; then
+	echo "fizz buzz"
+else
+	echo my_number
+fi
